919b149d5a220f5e312b3cfab6311ed8acc15d9b
espedine <irwinchee.hn@gmail.com>
2018-03-21 12:55:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -249,7 +249,7 @@
     <h3>Where Users Earn from Group Chats They Create</h3>
     <br>
     <br>
-    <a href="https://goo.gl/zg68sa" target="_blank" class="btn light-btn">Whitepaper</a> &nbsp;&nbsp;<a href="https://i.diawi.com/t5qFwv" target="_blank" class="android"><img src="img/android-btn.png" alt=""></a>
+    <a href="https://goo.gl/zg68sa" target="_blank" class="btn light-btn">Whitepaper</a> &nbsp;&nbsp;<a href="https://i.diawi.com/t5qFwv" target="_blank" class="android" width="50px"><img src="img/android-btn.png" alt=""></a>
     <br>
     <br>
     <br>
